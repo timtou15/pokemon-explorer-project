956f0fb8054ff6af8ab17c8ff93d39a0c5ebcd6c
Luiz Carlos <42881787+timtou15@users.noreply.github.com>
2025-06-21 06:14:58 -0300

    fix: envolver todos os componentes com NavigationProvider no App.tsx
    Reestruturado o App.tsx para garantir que todos os componentes da aplicação estejam corretamente envolvidos pelo NavigationProvider, evitando erros de contexto relacionados ao uso do hook useNavigation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { LanguageProvider } from './context/LanguageContext';
+import { NavigationProvider } from './context/NavigationContext';
 import { PokemonList } from './pages/PokemonList';
 import { PokemonDetail } from './pages/PokemonDetail';
 import { LanguageToggle } from './components/LanguageToggle';
@@ -10,13 +11,17 @@ function App() {
   return (
     <LanguageProvider>
       <FavoritesProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<PokemonList />} />
-            <Route path="/pokemon/:id" element={<PokemonDetail />} />
-          </Routes>
-          <LanguageToggle />
-        </Router>
+        <NavigationProvider>
+          <Router>
+            <div className="relative">
+              <Routes>
+                <Route path="/" element={<PokemonList />} />
+                <Route path="/pokemon/:id" element={<PokemonDetail />} />
+              </Routes>
+              <LanguageToggle />
+            </div>
+          </Router>
+        </NavigationProvider>
       </FavoritesProvider>
     </LanguageProvider>
   );
