4fb6ae8e3698e4c5b79957fbe39f8069bdfe6d9b
Luiz Carlos <42881787+timtou15@users.noreply.github.com>
2025-06-23 19:10:38 -0300

    refactor: reorganizar hierarquia de contextos no App.tsx
    Adicionado o ComparisonProvider à hierarquia de contextos, garantindo o gerenciamento correto de estado global da comparação.

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { LanguageProvider } from './context/LanguageContext';
 import { NavigationProvider } from './context/NavigationContext';
+import { ComparisonProvider } from './context/ComparisonContext';
 import { PokemonList } from './pages/PokemonList';
 import { PokemonDetail } from './pages/PokemonDetail';
 import { LanguageToggle } from './components/LanguageToggle';
+import { ComparisonPanel } from './components/ComparisonPanel';
 
 function App() {
   return (
     <LanguageProvider>
       <FavoritesProvider>
-        <NavigationProvider>
-          <Router>
-            <div className="relative">
-              <Routes>
-                <Route path="/" element={<PokemonList />} />
-                <Route path="/pokemon/:id" element={<PokemonDetail />} />
-              </Routes>
-              <LanguageToggle />
-            </div>
-          </Router>
-        </NavigationProvider>
+        <ComparisonProvider>
+          <NavigationProvider>
+            <Router>
+              <div className="relative">
+                <Routes>
+                  <Route path="/" element={<PokemonList />} />
+                  <Route path="/pokemon/:id" element={<PokemonDetail />} />
+                </Routes>
+                <LanguageToggle />
+                <ComparisonPanel />
+              </div>
+            </Router>
+          </NavigationProvider>
+        </ComparisonProvider>
       </FavoritesProvider>
     </LanguageProvider>
   );
